2f3ac4c45b288f6868f346f88c3e566b91933b96
kirkaclo555 <kirkaclo555@users.noreply.github.com>
2026-09-21 03:53:12 +0800

    fix(supabase): add fallback values for NEXT_PUBLIC_SUPABASE_URL and KEY to prevent build failure on Vercel

--- apps/web/lib/supabaseClient.ts
+++ apps/web/lib/supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  'https://skgcqpoxdatddehvgskf.supabase.co';
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  'sb_publishable_xzZ9lkZq3DdGcI1l0rnnFQ_5C5cKhD7';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
